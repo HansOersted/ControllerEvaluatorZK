cfd4da74752de24eefa401c83b5f809ec1802163
Hans Oersted <lucidshenzhe@gmail.com>
2025-05-16 22:53:53 +0200

    controller evaluator corrected

--- epsilon_estimation.py
+++ epsilon_estimation.py
@@ -1,63 +1,27 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
-# load previously trained A from stability_proof output
+# load A from stability_proof output
 model_data = np.load('final_model_weights.npz')
 A = model_data['A']
 
-# training config
-n_training = 2
+# load training data from interested_section output
+data = np.load('interested_section_output.npz')
+e_interested = data['interested_TrackingError']
+de_interested = data['interested_der_interested_TrackingError']
+dde_interested = data['der_der_interested_TrackingError']
+
+# reshape and prepare data
 lambda_val = 5
-sample_time = 0.002
 
-# load CSV data
-df = pd.read_csv('final_data.csv')
+E_interested = np.stack((de_interested, dde_interested), axis=1)
 
-time = df['timestamp'].to_numpy()
-q_ref_first = df['target_q_3'].to_numpy()
-dq_ref_first = df['target_qd_3'].to_numpy()
-q_ref_second = df['target_q_5'].to_numpy()
-dq_ref_second = df['target_qd_5'].to_numpy()
-
-q_actual_first = df['actual_q_3'].to_numpy()
-dq_actual_first = df['actual_qd_3'].to_numpy()
-q_actual_second = df['actual_q_5'].to_numpy()
-dq_actual_second = df['actual_qd_5'].to_numpy()
-
-e_csv_first = q_actual_first - q_ref_first
-de_csv_first = dq_actual_first - dq_ref_first
-e_csv_second = q_actual_second - q_ref_second
-de_csv_second = dq_actual_second - dq_ref_second
-
-dde_csv_first = np.diff(e_csv_first) / sample_time
-dde_csv_second = np.diff(e_csv_second) / sample_time
-
-# valid nontrivial indices
-nontrival_idx = np.array([...])  # ← Fill in with your full 99x2 index array
-
-# collect training index
-training_index = []
-for i in range(n_training):
-    training_index.extend(range(nontrival_idx[i, 0], nontrival_idx[i, 1] + 1))
-training_index = np.array(training_index)
-
-# collect training data (only 1st derivative, consistent with stability_proof)
-de_training_first = de_csv_first[training_index]
-de_training_second = de_csv_second[training_index]
-dde_training_first = dde_csv_first[training_index]
-dde_training_second = dde_csv_second[training_index]
-
-de_training = np.stack((de_training_first, de_training_second), axis=1)
-dde_training = np.stack((dde_training_first, dde_training_second), axis=1)
-
-# compute constraints
 constraint_values = []
-for de, dde in zip(de_training, dde_training):
-    de = de.reshape(-1, 1)
-    dde = dde.reshape(-1, 1)
-    c = dde.T @ A @ de + de.T @ A @ dde + lambda_val * de.T @ A @ de
-    constraint_values.append(c.item())
+for row in E_interested:
+    de = np.array([[row[0]], [row[1]]])
+    dde = np.array([[row[1]], [0]])
+    c = (dde.T @ A @ de + de.T @ A @ dde + lambda_val * de.T @ A @ de).item()
+    constraint_values.append(c)
 
 # plot constraints
 plt.figure()
